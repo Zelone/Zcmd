fc565d206842d9785b5ba92d585d02288de3a94f
Zelone <jieshmob1@gmail.com>
2019-12-23 20:05:26 +0530

    second set

--- gitit.cmd
+++ gitit.cmd
@@ -1,11 +1,9 @@
 set now=%cd%
 cd /d %Zel%
 cd Zelone
-git init 
 git add *.cmd
 git add *.md
 git add *.exe
-git commit -m "first commit"
-git remote add origin git@github.com:Zelone/Zcmd.git
+git commit -m "%*"
 git push -u origin master
 cd /d %now%
